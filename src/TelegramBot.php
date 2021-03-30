d33650a0dabf0386542a2fb5f5f69689d2b8cc93
Miroslav Štolfa <miroslav.stolfa@ataccama.com>
2021-03-30 17:47:42 +0200

    helper for obtaining chat ID

--- src/TelegramBot.php
+++ src/TelegramBot.php
@@ -32,4 +32,21 @@
             $url = $bot_url . "sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message);
             file_get_contents($url);
         }
+
+        /**
+         * Returns updates.
+         *
+         * You can use it for obtaining Chat ID of the channel/group.
+         *
+         * @return array
+         */
+        public function getUpdates(): array
+        {
+            $data = file_get_contents("https://api.telegram.org/bot$this->botToken/getUpdates");
+            if (!empty($data)) {
+                return json_decode($data, true);
+            }
+
+            return [];
+        }
     }
